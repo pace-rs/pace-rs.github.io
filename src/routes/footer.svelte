900a51767540ab9f9ad8052e9ad0f504fbe7b346
dj0le <gstar12@hotmail.com>
2024-02-21 13:28:57 +0100

    updated logo and brand marks

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -1,18 +1,28 @@
 <script lang="ts">
 	import * as config from '$lib/config';
 	import { DiscordLogo, GithubLogo } from 'phosphor-svelte';
+	import { theme } from '$lib/theme';
 </script>
 
-<div class="card">
+<div class="flex">
+	<div class="card">
+		<div>
+			<p>
+				copyright &copy <span class="highlight">PACE</span>
+				{new Date().getFullYear()} // all rights reserved
+			</p>
+			<p>app: <span class="highlight">Simonsan</span></p>
+			<p>
+				front end: <span class="highlight"><a href="https://space-shift.com">SPACEshift</a></span>
+			</p>
+		</div>
+	</div>
 	<div>
-		<p>
-			copyright &copy <span class="highlight">PACE</span>
-			{new Date().getFullYear()} // all rights reserved
-		</p>
-		<p>app: <span class="highlight">Simonsan</span></p>
-		<p>
-			front end: <span class="highlight"><a href="https://space-shift.com">SPACEshift</a></span>
-		</p>
+		<img
+			class="brand"
+			src={$theme === 'dark' ? './Brand-Dark.png' : './Brand-Light.png'}
+			alt="PACE"
+		/>
 	</div>
 </div>
 <footer>
@@ -28,14 +38,17 @@
 </footer>
 
 <style>
+	.flex {
+		display: flex;
+		justify-content: space-between;
+		align-items: flex-end;
+	}
 	footer {
 		display: flex;
 		justify-content: space-between;
-		/* padding-block: var(--size-6); */
 		padding-inline: 5rem;
 		border-radius: var(--size-3) var(--size-3) 0 0;
 		background-color: var(--accent-4);
-		padding-block: 1rem;
 	}
 	.footer-nav {
 		padding-left: 1rem;
@@ -44,9 +57,10 @@
 		justify-items: start;
 		align-items: center;
 		gap: 2rem;
+		padding-top: 1rem;
 	}
 	a {
-		color: inherit;
+		color: var(--text-footer);
 	}
 	p {
 		color: var(--text-footer);
@@ -64,25 +78,36 @@
 	.card p {
 		color: var(--text-1);
 	}
+	.card a {
+		color: var(--accent-5);
+	}
 	.highlight {
-		color: var(--accent-1);
+		color: var(--accent-5);
 		font-family: var(--font-title);
+	}
+	.brand {
+		padding-right: 8rem;
+		margin-bottom: -1rem;
+		position: relative;
+		z-index: 2;
 	}
 	.tag-grid {
 		display: grid;
 		grid-template-columns: 1fr 1fr;
 		justify-items: center;
-		align-items: stretch;
 		padding-right: 1rem;
 	}
 	.logo-tagline {
 		font-family: var(--font-title);
 		font-size: 3rem;
+		padding-top: 1rem;
 	}
 	.logo-text {
 		font-size: 3rem;
 		color: var(--accent-1);
 		background-color: var(--background);
 		padding-inline: 1rem;
+		align-self: stretch;
+		padding-top: 1rem;
 	}
 </style>
