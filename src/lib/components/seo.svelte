294241bfb8fc8551c29ed11fc8c49af658e041a6
dj0le <gstar12@hotmail.com>
2024-02-29 20:00:14 +0100

    add SEO functionality

--- src/lib/components/seo.svelte
+++ src/lib/components/seo.svelte
@@ -6,7 +6,7 @@
 	<title>{title}</title>
 	<meta name="description" content={description} />
 	<meta property="og:site_name" content="PACE" />
-	<meta property="“og:title”" content="Keep the PACE. Time tracking on the command line" />
+	<meta property="“og:title”" content="PACE // cli time tracker //" />
 	<meta property="og:description" content="${description}" />
 	<meta property="og:url" content="https://pace.cli.rs/" />
 	<meta property="og:type" content="website" />
@@ -14,10 +14,13 @@
 	<meta property="og:image:type" content="image/png" />
 	<meta property="og:image:width" content="1280" />
 	<meta property="og:image:height" content="627" />
-	<meta property="og:image" content="https://pace.cli.rs/Pace-Square-Logo.png" />
-	<meta property="og:image:type" content="image/png" />
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:image" content="https://pace.cli.rs/Pace-Square-Logo.png" />
+	<meta name="og:image" content="https://pace.cli.rs/Pace-Square-Logo.png" />
 	<meta property="og:image:width" content="400" />
 	<meta property="og:image:height" content="400" />
+	<meta name="twitter:title" content="PACE // cli time tracker //" />
+	<meta name="twitter:description" content="Keep the PACE. Time tracking on the command line" />
 
 	{@html `<script type="application/ld+json">
 		{
